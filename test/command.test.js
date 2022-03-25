547dce8b389b88ce78e55f8e48032653a22b74ac
Eric Hart <>
2022-03-25 18:21:34 +0800

    [Add]

--- test/command.test.js
+++ test/command.test.js
@@ -5,16 +5,15 @@ function buildStatic(project, operates, paths, shells) {
   let srcPath;
   switch (project) {
     case 'drip-cli':
-      srcPath = path.join('.');
+      srcPath = '.';
       break;
     default:
       srcPath = path.join('..', project);
       break;
   }
-  console.log('srcPath', srcPath);
   const cwd = process.cwd();
   const staticPath = path.join('/', 'tmp', project + '-staitc');
-  shells.push('cd ', srcPath);
+  shells.push('cd ' + srcPath);
   operates.forEach((p) => {
     shells.push('yarn run ' + p);
   });
@@ -28,6 +27,7 @@ function buildStatic(project, operates, paths, shells) {
   shells.push('rm -rf .git');
   shells.push('git init');
   shells.push('git add --all');
+  shells.push('git gc --force');
   shells.push('git commit -m "[Update]"');
   shells.push('cd ' + cwd)
 }
@@ -36,7 +36,7 @@ beforeAll(() => {
   const shells = [];
   shells.push('rm -rf $HOME/.drip/');
   buildStatic('drip-cli', ['build'], ['bin', 'dist', 'asset', 'node_modules'], shells);
-  buildStatic('drip-local', ['build', 'pro'], ['bin', 'dist', 'asset', 'node_modules'], shells);
+  buildStatic('drip-local', ['build', 'pro'], ['dist', 'node_modules'], shells);
   buildStatic('drip-package-shell', ['build', 'pro'], ['dist'], shells);
   shells.push('node ./dist/bin/install.js');
   shells.push('rm -rf /tmp/example');
@@ -44,18 +44,14 @@ beforeAll(() => {
   execSync(shells.join('&&'));
 });
 
-afterAll(() => {
-  //execSync('rm -rf /tmp/example');
-});
-
 test('main process', () => {
-  //execSync([
-    //'cd /tmp/example/',
-    //'unset PREFIX',
-    //'. ~/.nvm/nvm.sh',
-    //'nvm use v17.3.0',
-    //'drip init -y',
-    //'drip install',
-    //'echo \'locate index\' > /tmp/example/.drip/local/instance/[shell]:shell1',
-  //].join(' && '));
+  const shells = [];
+  shells.push('cd /tmp/example/');
+  shells.push('unset PREFIX');
+  shells.push('. ~/.nvm/nvm.sh');
+  shells.push('nvm use v17.3.0');
+  shells.push('drip init -y');
+  shells.push('drip install');
+  shells.push('echo \'locate index\' > .drip/local/instance/[shell]:shell1');
+  execSync(shells.join('&&'));
 });

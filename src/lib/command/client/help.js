504094046f6bfb3e92e0402c2923b648f3ca8c72
Eric Hart <>
2023-01-09 17:22:36 +0800

    [Add]

--- src/lib/command/client/help.js
+++ src/lib/command/client/help.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 
 export default function help(...param) {
   console.log([
-    '[' + chalk.bold('drip server') + '] command line tip:',
+    '[' + chalk.bold('drip server') + chalk.bold('] command line tip') + ':',
     '',
     '-' + chalk.bold('p') + ', --' + chalk.bold('port') + '  Appoint drip sever run concrete socket port.',
   ].join('\n'));
--- src/lib/command/server/help.js
+++ src/lib/command/server/help.js
@@ -1,8 +1,10 @@
 import chalk from 'chalk';
 
+import chalk from 'chalk';
+
 export default function help(...param) {
   console.log([
-    '[' + chalk.bold('drip client') + '] command line tip:',
+    '[' + chalk.bold('drip server') + chalk.bold('] command line tip') + ':',
     '',
     '-' + chalk.bold('p') + ', --' + chalk.bold('port') + '  Appoint drip client connect concrete socket port.',
   ].join('\n'));

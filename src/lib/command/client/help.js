ebd906539090a8b4767f7c14d2d4251195adb474
Eric Hart <>
2023-01-09 17:20:09 +0800

    [Add]

--- src/lib/command/client/help.js
+++ src/lib/command/client/help.js
@@ -1,3 +1,9 @@
+import chalk from 'chalk';
+
 export default function help(...param) {
-  const [pkg, ...rest] = param;
+  console.log([
+    '[' + chalk.bold('drip server') + '] command line tip:',
+    '',
+    '-' + chalk.bold('p') + ', --' + chalk.bold('port') + '  Appoint drip sever run concrete socket port.',
+  ].join('\n'));
 }
--- src/lib/command/server/help.js
+++ src/lib/command/server/help.js
@@ -1,3 +1,9 @@
+import chalk from 'chalk';
+
 export default function help(...param) {
-  const [pkg, ...rest] = param;
+  console.log([
+    '[' + chalk.bold('drip client') + '] command line tip:',
+    '',
+    '-' + chalk.bold('p') + ', --' + chalk.bold('port') + '  Appoint drip client connect concrete socket port.',
+  ].join('\n'));
 }

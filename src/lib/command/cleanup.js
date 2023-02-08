b7ea7c3c87386858c14204ee702b6bd0c47c0053
Eric Hart <>
2023-02-09 00:17:03 +0800

    [Add]

--- src/lib/command/cleanup.js
+++ src/lib/command/cleanup.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { execSync, } from 'child_process';
 import iteratorPackagePath from '~/lib/util/iteratorPackagePath';
 import getVersionHash from '~/lib/util/getVersionHash';
+import getLatestVersion from '~/lib/util/getLatestVersion';
 
 export default function cleanup(...param) {
   const packagePath = path.join('.drip', 'local', 'package');
@@ -11,7 +12,10 @@ export default function cleanup(...param) {
     const pkgPath = path.join(packagePath, p);
     let shells = [];
     shells.push('cd ' + pkgPath);
-    const version = versionHash[p];
+    let version = versionHash[p];
+    if (version === '') {
+      version = getLatestVersion(pkgPath);
+    }
     shells.push('git checkout ' + version);
     shells.push('git stash');
     fs.rmSync(path.join(pkgPath, '.git'), { recursive: true, });

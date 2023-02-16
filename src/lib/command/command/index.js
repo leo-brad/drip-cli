b0369e6ae14eacd85660890212b74763df68c20a
Eric Hart <>
2023-02-17 00:06:54 +0800

    [Add]

--- src/lib/command/command/index.js
+++ src/lib/command/command/index.js
@@ -3,12 +3,12 @@ import chalk from 'chalk';
 import checkPath from '~/lib/util/checkPath';
 import add from './add';
 import list from './list';
-import h from './help';
-import help from '~/lib/command/help';
+import help from './help';
+import h from '~/lib/command/help';
 
 export default async function command(...param) {
   const dripPath = path.join(process.env.HOME, '.drip');
-  checkPath(path.resolve('.drip'), help);
+  checkPath(path.resolve('.drip'), h);
   const [command, ...rest] = param;
   switch (command) {
     case 'add':

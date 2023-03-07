127237f0edcd135463e818858fe8e0a2d359d869
Eric Hart <>
2023-03-07 22:08:15 +0800

    [Add]

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-//import { spawn, } from 'child_process';
-//import fs from 'fs';
-//import path from 'path';
-//import parseConfig from '~/lib/util/parseConfig';
+import { spawn, } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import parseConfig from '~/lib/util/parseConfig';
 
-//const local = parseConfig('./asset/.drip/local/config');
-//const project = parseConfig('./asset/.drip/project/config');
-//const config = { ...project, ...local,  };
-//const localPath = path.join(process.env.HOME, '.drip', 'command', 'local');
-//if (fs.existsSync(localPath)) {
-  //process.chdir(localPath);
-  //spawn(
-    //'npx',
-    //['electron', 'dist/main.js', JSON.stringify(config), '/tmp/example/'],
-    //{ detached: true, },
-  //);
-//}
-import start from '~/lib/command/client/main.js';
-
-process.chdir('/tmp/example/');
-start();
+const local = parseConfig('./asset/.drip/local/config');
+const project = parseConfig('./asset/.drip/project/config');
+const config = { ...project, ...local,  };
+const localPath = path.join(process.env.HOME, '.drip', 'command', 'local');
+if (fs.existsSync(localPath)) {
+  process.chdir(localPath);
+  spawn(
+    'npx',
+    ['electron', 'dist/main.js', JSON.stringify(config), '/tmp/example/'],
+    { detached: true, },
+  );
+}

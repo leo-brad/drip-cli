02f8b459ed57f47d985a57862cda8ec7d6435490
Eric Hart <>
2023-04-24 15:46:10 +0800

    [Add]

--- src/lib/command/package/main.js
+++ src/lib/command/package/main.js
@@ -2,9 +2,8 @@ import { spawn, } from 'child_process';
 import path from 'path';
 
 export default function main(...rest) {
-  const cwd = process.cwd();
-  process.chdir(path.join(process.env.HOME, '.drip', 'command', 'package'));
   spawn(
-    'node', ['dist/index.js', ...rest], { stdio: 'inherit', },
+    'node', [path.join(process.env.HOME, '.drip', 'command', 'package', 'dist', 'index.js'), ...rest],
+    { stdio: 'inherit', detached: true, },
   );
 }
